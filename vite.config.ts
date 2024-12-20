4abd60bd6ea709e2f017c02530f6e502e60a756f
MaxFirst <2577870094@qq.com>
2024-12-20 17:31:34 +0800

    version2.0 适配vercel

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,26 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['vue-router', 'pinia']
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_MODE === '1' 
+          ? process.env.VITE_REMOTE_API_URL 
+          : process.env.VITE_LOCAL_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'element-plus': ['element-plus']
+        }
+      }
+    }
   }
 })
